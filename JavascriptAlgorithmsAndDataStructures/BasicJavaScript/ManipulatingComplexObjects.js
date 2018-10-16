a1281ae1752aa9e3ba8803e4f2891df810738e4c
unknown <sksaaju@gmail.com>
2018-10-16 03:16:34 -0700

    Solution of JavaScript Object

---
+++ JavascriptAlgorithmsAndDataStructures/BasicJavaScript/ManipulatingComplexObjects.js
@@ -0,0 +1,25 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Sk Saju",
+    "title": "Piano Man",
+    "release_year": 2018,
+    "formats": [ 
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+  // Add record here
+];
