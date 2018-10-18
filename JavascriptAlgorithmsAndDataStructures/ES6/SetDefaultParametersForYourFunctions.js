bea2faa772c424f4c0610925952fd07668bc4a1f
unknown <sksaaju@gmail.com>
2018-10-18 00:26:31 -0700

    Rest and Spread Operator

--- JavascriptAlgorithmsAndDataStructures/ES6/SetDefaultParametersForYourFunctions.js
+++ JavascriptAlgorithmsAndDataStructures/ES6/SetDefaultParametersForYourFunctions.js
@@ -1,14 +1,8 @@
-const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
-
-const squareList = (arr) => {
+const increment = (function() {
   "use strict";
-  // change code below this line
-  const squaredIntegers = arr
-    .filter(item => item>0 && item==parseInt(item))
-    .map(item => item * item);
-  // change code above this line
-  return squaredIntegers;
-};
-// test your code
-const squaredIntegers = squareList(realNumberArray);
-console.log(squaredIntegers);
+  return function increment(number, value=1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns 6
