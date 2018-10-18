9015580e8339fc33c973d5b5d9b1099493dc386a
unknown <sksaaju@gmail.com>
2018-10-18 03:27:33 -0700

    Using the Test Method

--- JavascriptAlgorithmsAndDataStructures/03-RegularExpressions/01-UsingTheTestMethod.js
+++ JavascriptAlgorithmsAndDataStructures/03-RegularExpressions/01-UsingTheTestMethod.js
@@ -1,10 +1,3 @@
-function checkScope() {
-  "use strict";
-    let i = "function scope";
-    if (true) {
-      let i = "block scope";
-      console.log("Block scope i is: ", i);
-    }
-    console.log("Function scope i is: ", i);
-    return i;
-  }
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
