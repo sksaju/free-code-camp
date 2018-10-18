7a31672b484c00b9f7ce362c8b21dfc06792bec3
unknown <sksaaju@gmail.com>
2018-10-18 01:57:23 -0700

    Template Literals

---
+++ JavascriptAlgorithmsAndDataStructures/ES6/CreateStringsUsingTemplateLiterals.js
@@ -0,0 +1,23 @@
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+  const resultDisplayArray = arr.map(
+        item => `<li class="text-warning">${item}</li>`
+      );
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`, 
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
