0ce7ad69a55a5a3bf4d477bbe1ab6edb506f0f7a
unknown <sksaaju@gmail.com>
2018-10-18 02:43:42 -0700

    Class

---
+++ JavascriptAlgorithmsAndDataStructures/ES6/UseClassSyntaxToDefineConstructorFunction.js
@@ -0,0 +1,9 @@
+class Vegetable {
+  "use strict";
+  /* Alter code below this line */
+  constructor(name){
+    this.name = name;
+  }
+}
+const carrot = new Vegetable('carrot');
+console.log(carrot.name); // => should be 'carrot'
