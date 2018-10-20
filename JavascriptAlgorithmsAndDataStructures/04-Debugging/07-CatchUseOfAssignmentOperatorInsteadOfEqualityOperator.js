43b82e9ee9cef5a6de63b5ecb4e4fc1309ec104b
unknown <sksaaju@gmail.com>
2018-10-19 22:17:23 -0700

    Debugging

--- JavascriptAlgorithmsAndDataStructures/04-Debugging/07-CatchUseOfAssignmentOperatorInsteadOfEqualityOperator.js
+++ JavascriptAlgorithmsAndDataStructures/04-Debugging/07-CatchUseOfAssignmentOperatorInsteadOfEqualityOperator.js
@@ -1,2 +1,11 @@
-let innerHtml = "<p>Click here to <a href='#Home'>return home</a></p>";
-console.log(innerHtml);
+let x = 7;
+let y = 9;
+let result = "to come";
+
+if(x == y) {
+  result = "Equal!";
+} else {
+  result = "Not equal!";
+}
+
+console.log(result);
