250195701634b27b5daba0fb0734307c96951365
unknown <sksaaju@gmail.com>
2018-10-21 22:33:25 -0700

    Data Structure

---
+++ JavascriptAlgorithmsAndDataStructures/05-BasicDataStructures/12-CreateComplexMultiDimensionalArrays.js
@@ -0,0 +1,18 @@
+let myNestedArray = [
+  // change code below this line
+  ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+  [
+    ['loop', 'shift', 6, 7, 1000, 'method'], ['deep']
+  ],
+  [
+    [
+      ['concat', false, true, 'spread', 'array'], ['deeper']
+    ],
+    [
+      [
+        ['mutate', 1327.98, 'splice', 'slice', 'push'], ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth'], ['deepest']
+      ],
+    ]
+  ]
+  // change code above this line
+];
