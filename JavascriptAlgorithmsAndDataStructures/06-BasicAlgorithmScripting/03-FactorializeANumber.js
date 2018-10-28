cf997d25e1fa2661da06a73f77faec45f70f805f
unknown <sksaaju@gmail.com>
2018-10-27 23:02:44 -0700

    Basic Algorithm Scripting

--- JavascriptAlgorithmsAndDataStructures/06-BasicAlgorithmScripting/03-FactorializeANumber.js
+++ JavascriptAlgorithmsAndDataStructures/06-BasicAlgorithmScripting/03-FactorializeANumber.js
@@ -1,5 +1,11 @@
-function reverseString(str) {
-    return str.split("").reverse().join("");;
-}
-
-reverseString("hello");
+function factorialize(num) {
+    if (num < 0) { // If the number is less than 0, reject it.
+          return -1;
+    } else if (num == 0) {  // If the number is 0, its factorial is 1.
+        return 1;
+    }  else { // Otherwise, call the recursive procedure again
+      return (num * factorialize(num - 1));
+    }
+  }
+  
+  factorialize(5);
