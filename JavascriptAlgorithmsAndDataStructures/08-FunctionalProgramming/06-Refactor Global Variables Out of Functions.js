f603e6bb42c58ed82ffe2142707d9cbf9645bbe2
unknown <sksaaju@gmail.com>
2018-11-06 01:54:38 -0800

    Functional Programming

---
+++ JavascriptAlgorithmsAndDataStructures/08-FunctionalProgramming/06-Refactor Global Variables Out of Functions.js
@@ -0,0 +1,26 @@
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function add (arr, bookName) {
+  return [...arr, bookName];
+  
+  // Add your code above this line
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+// Add your code below this line
+function remove (arr, bookName) {
+  return arr.filter(book => book !== bookName);
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
