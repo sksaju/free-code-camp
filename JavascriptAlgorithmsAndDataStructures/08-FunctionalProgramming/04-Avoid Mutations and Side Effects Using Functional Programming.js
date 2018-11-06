8382eb61424d4e2c941ef7d45b05eea135e38750
unknown <sksaaju@gmail.com>
2018-11-06 01:26:52 -0800

    Functional Programming

---
+++ JavascriptAlgorithmsAndDataStructures/08-FunctionalProgramming/04-Avoid Mutations and Side Effects Using Functional Programming.js
@@ -0,0 +1,12 @@
+// the global variable
+var fixedValue = 4;
+
+function incrementer () {
+  // Add your code below this line
+  
+  return fixedValue+1;
+  // Add your code above this line
+}
+
+var newValue = incrementer(); // Should equal 5
+console.log(fixedValue); // Should print 4
---
+++ JavascriptAlgorithmsAndDataStructures/08-FunctionalProgramming/05-Pass Arguments to Avoid External Dependence in a Function.js
@@ -0,0 +1,12 @@
+// the global variable
+var fixedValue = 4;
+
+// Add your code below this line
+function incrementer (arg) {
+  
+  return arg+1;
+  // Add your code above this line
+}
+
+var newValue = incrementer(fixedValue); // Should equal 5
+console.log(fixedValue); // Should print 4
