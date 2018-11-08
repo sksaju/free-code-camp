a2a8614ecb076bfc985754e5ecd4baec56645afe
unknown <sksaaju@gmail.com>
2018-11-08 02:12:37 -0800

    Functional Programming

---
+++ JavascriptAlgorithmsAndDataStructures/08-FunctionalProgramming/22=Use the some Method to Check that Any Elements in an Array Meet a Criteria.js
@@ -0,0 +1,8 @@
+function checkPositive(arr) {
+  // Add your code below this line
+  
+  return arr.some( val => val>0 );
+  
+  // Add your code above this line
+}
+checkPositive([1, 2, 3, -4, 5]);
