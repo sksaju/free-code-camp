16156dfe71dc669d4f19dbc55a60d034b6ddf8c1
unknown <sksaaju@gmail.com>
2018-11-09 13:02:20 -0800

    Intermediate Algorithm Scripting

---
+++ JavascriptAlgorithmsAndDataStructures/09-IntermediateAlgorithmScripting/10-Convert HTML Entities.js
@@ -0,0 +1,15 @@
+function convertHTML(str) {
+  // Use Object Lookup to declare as many HTML entities as needed.
+  var htmlEntities = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    '\'': "&apos;"
+  };
+  //Use map function to return a filtered str with all entities changed automatically.
+  return str.split('').map(entity => htmlEntities[entity] || entity).join('');
+}
+
+// test here
+convertHTML("Dolce & Gabbana");
