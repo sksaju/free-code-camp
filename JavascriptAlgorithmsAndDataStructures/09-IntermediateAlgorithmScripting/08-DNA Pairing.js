994d751dde6b31501486d3e1ea296a09498fe142
unknown <sksaaju@gmail.com>
2018-11-09 12:55:59 -0800

    Intermediate Algorithm Scripting

--- JavascriptAlgorithmsAndDataStructures/09-IntermediateAlgorithmScripting/08-DNA Pairing.js
+++ JavascriptAlgorithmsAndDataStructures/09-IntermediateAlgorithmScripting/08-DNA Pairing.js
@@ -1,16 +1,16 @@
-function myReplace(str, before, after) {
-  
-  // Find index where before is on string
-  var index = str.indexOf(before);
-  // Check to see if the first letter is uppercase or not
-  if (str[index] === str[index].toUpperCase()) {
-    // Change the after word to be capitalized before we use it.
-    after = after.charAt(0).toUpperCase() + after.slice(1);
+function pairElement(str) {
+  //create object for pair lookup
+  var pairs = {
+    "A": "T",
+    "T": "A",
+    "C": "G",
+    "G": "C"
   }
-  // Now replace the original str with the edited one.
-  str = str.replace(before, after);
-
-  return str;
+  //split string into array of characters
+  var arr = str.split("");
+  //map character to array of character and matching pair
+  return arr.map(x => [x,pairs[x]]);
 }
 
-myReplace("A quick brown fox jumped over the lazy dog", "jumped", "leaped");
+//test here
+pairElement("GCG");
