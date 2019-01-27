e55d64d2fa407ea9227bc50eb807cacba646ecc5
unknown <sksaaju@gmail.com>
2019-01-27 16:35:21 +0600

    Front-End Library ReactJS

---
+++ front-end-libraries/react/02- Create a Complex JSX Element.js
@@ -0,0 +1,12 @@
+
+const JSX = (
+    <div>
+        <h1>Heading one</h1>
+        <p>Paragraph One</p>
+        <ul>
+            <li> item one </li>
+            <li> item two </li>
+            <li> item three </li>
+        </ul>
+    </div>
+);
---
+++ front-end-libraries/react/03- Add Comments in JSX.js
@@ -0,0 +1,7 @@
+const JSX = (
+    <div>
+      {/* block item */}
+      <h1>This is a block of JSX</h1>
+      <p>Here's a subtitle</p>
+    </div>
+  );
