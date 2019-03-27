6e2c62f00c3a62cb3c77fbb9d074fd7b325f8885
unknown <sksaaju@gmail.com>
2019-03-27 23:41:40 +0600

    Front-End Library ReactJS

---
+++ front-end-libraries/react/12- Render a Class Component to the DOM.js
@@ -0,0 +1,17 @@
+class TypesOfFood extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        <Fruits  />
+        <Vegetables  />
+      </div>
+    );
+  }
+};
+
+// change code below this line
+ReactDOM.render(<TypesOfFood />, document.getElementById('challenge-node'));
